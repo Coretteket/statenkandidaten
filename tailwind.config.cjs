6275743afef56bbd9eca80c34a63a3a276f48972
Quinten Coret <Qcoret@gmail.com>
2023-01-29 16:40:48 +0100

    modify tailwind config & styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,23 @@
 const { fontFamily } = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: { extend: { fontFamily: { sans: ['var(--base-font)', fontFamily.sans] } } },
+	theme: {
+		extend: {
+			fontFamily: { sans: ['var(--base-font)', fontFamily.sans] },
+			screens: { '2xs': '360px', xs: '380px', lg: '900px', xl: '1024px', '2xl': '1280px' },
+			gridTemplateColumns: { 'content-sidebar': '1fr 20rem' },
+		},
+	},
 	future: { hoverOnlyWhenSupported: true },
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/forms')({ strategy: 'class' }),
+		plugin(({ addVariant }) => {
+			addVariant('tap', ['@media(hover: hover) and (pointer: fine) { &:hover }', '&:active']);
+			addVariant('kid', '&>*');
+			addVariant('kids', '& *');
+		}),
+	],
 };
