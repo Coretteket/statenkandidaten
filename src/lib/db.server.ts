41f6777368671cdcbf333030b7a83781363e12cd
Quinten Coret <Qcoret@gmail.com>
2023-02-25 22:45:22 +0100

    context-dependent prisma client

--- src/lib/db.server.ts
+++ src/lib/db.server.ts
@@ -1,6 +1,9 @@
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient as PrismaClientNode } from '@prisma/client';
+import { PrismaClient as PrismaClientEdge } from '@prisma/client/edge';
 
-const globalPrisma = globalThis as typeof globalThis & { prisma?: PrismaClient };
+const PrismaClient = import.meta.env.DEV ? PrismaClientNode : PrismaClientEdge;
+
+const globalPrisma = globalThis as typeof globalThis & { prisma?: PrismaClientNode };
 const prisma = globalPrisma.prisma ?? new PrismaClient({ log: ['error', 'warn'] });
 if (import.meta.env.DEV) globalPrisma.prisma = prisma;
 
