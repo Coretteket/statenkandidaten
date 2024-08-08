381cafb5442b1452983bdd5513248649c2a72a02
Quinten Coret <Qcoret@gmail.com>
2024-08-08 16:01:32 +0200

    Await promise

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,7 @@ import type { PageServerLoad } from './$types';
 import { prisma } from '~/lib/db.server';
 
 export const load: PageServerLoad = async () => {
-	const provinces = prisma.province.findMany({ orderBy: { name: 'asc' } });
+	const provinces = await prisma.province.findMany({ orderBy: { name: 'asc' } });
 	const meta = createMeta({ title: createTitle('Vind kandidaten in jouw provincie') });
 	return { provinces, meta };
 };
